c48c64e27a89192d85375d3a0ae1691ae2898135
Nylbert Victor <nyldrift@hotmail.com>
2023-01-12 22:03:25 +0000

    https://www.hackerrank.com/challenges/staircase/problem

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
-function convertHTML(str) {
-  let chars="&<>\"\'",str2="";
-  for(let i=0;i<str.length;i++)
-  {
-    if(chars.includes(str[i])==true)
-    {
-      switch(str[i]){
-        case "&":str2+="&amp;"
-          break;
-        case "<":
-          str2+="&lt;"
-          break;
-        case ">":
-          str2+="&gt;"
-          break;
-        case "\'":
-          str2+="&apos;"
-          break;
-        case "\"":
-          str2+="&quot;"
-          break;
-        default:
-          break
-      }
-    }
-    else
-    {
-      str2+=str[i];
-    }
-  }
-  return str2;
-}
 
-convertHTML("Dolce & Gabbana");
+function staircase(n) {
+  let m=1, o=n-1;
+  let str="";
+  for(let k = 0;k<n;k++)
+  {
+    for(let i = o;i>0;i--)
+      {
+        str+=" ";
+        
+      }
+    for(let j=0;j<m;j++)
+      {
+        str+="#";
+      }
+    o--;
+    m++;
+    console.log(str);
+    str="";
+  }
+}
+staircase(10);
